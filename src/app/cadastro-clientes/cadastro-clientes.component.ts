1e8f715de7acfff035e59714c29b9d82e0926a16
Antonio Maia <antoniomaia.work@hotmail.com>
2019-09-25 13:29:36 -0300

    Primeiro commit github

---
+++ src/app/cadastro-clientes/cadastro-clientes.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-cadastro-clientes',
+  templateUrl: './cadastro-clientes.component.html',
+  styleUrls: ['./cadastro-clientes.component.css']
+})
+export class CadastroClientesComponent implements OnInit {
+  formCadastro;
+  valoresForm;
+  conversao;
+  constructor(private fb: FormBuilder) { }
+  
+  ngOnInit() {
+      this.formCadastro = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      telefone: [''],
+      endereco: ['']
+    });
+    this.formCadastro.valueChanges.pipe(
+      debounceTime(1000))
+      .subscribe(res => {
+        console.log(res);
+        this.valoresForm = res;
+      });
+  }
+  cadastro(){
+    console.log(this.formCadastro.controls);
+    this.conversao = JSON.stringify(this.valoresForm);
+    localStorage.setItem('cadastro', this.conversao);
+   }
+  
+}
